41cbed213094241572b6f82f8253a3429481d066
Alex Ou <alexou@Snype-MacBook-Pro.local>
2026-03-07 15:50:16 -0500

    Reorganize repository into frontend and backend folders

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,6 +3,13 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
+  {
+    settings: {
+      next: {
+        rootDir: "frontend",
+      },
+    },
+  },
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
